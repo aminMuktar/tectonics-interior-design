258f1156436de431861485dfc4b1fecf51869a77
aminMuktar <aminmuktar41@gmail.com>
2024-12-17 12:13:00 +0300

    make navbar more transparent

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar:FC = () =>{
     return (
         <>
             
-            <header className="fixed top-0 w-full z-50 bg-white bg-opacity-50 backdrop-blur-lg ">
+            <header className="fixed top-0 w-full z-50 bg-black bg-opacity-50 backdrop-blur-lg ">
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-6">
             <div className="flex lg:flex-1">
                 <a href="/" className="-m-1.5 p-1.5">
@@ -40,15 +40,15 @@ export const Navbar:FC = () =>{
                 <img alt="" src="/logo.svg" className="h-10 w-10 rounded-full" />
                 </a>
             </div>
-            <p className="hidden font-bold text-lg lg:flex lg:gap-x-12">
+            <p className="hidden text-white font-bold text-lg lg:flex lg:gap-x-12">
                 Tectonics Architecture and Interior Design
             </p>
             <div className="lg:flex lg:flex-1 lg:justify-end"> 
                 <button
-                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center text-background gap-2 hover:bg-gray-100 text-sm sm:text-base h-8 sm:h-10 px-4 sm:px-5"
+                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center text-background gap-2 hover:bg-black text-sm sm:text-base h-8 sm:h-10 px-4 sm:px-5"
                     onClick={handleOpen}
                 >
-                    <MenuIcon fontSize="large"/>
+                    <MenuIcon fontSize="large" sx={{ color: "white" }}/>
                 </button>
                 
             </div>
